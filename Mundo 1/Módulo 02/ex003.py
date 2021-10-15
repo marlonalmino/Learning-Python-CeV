e68d5800279b82a6cb502ee77bf81fb70ca70109
Marlon Almino <alminomarlon@gmail.com>
2021-10-15 18:59:18 -0300

    adicionados exercicios 3 e 4 do modulo 02

---
+++ Mundo 1/Módulo 02/ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+numero_um = int(input('Digite o primeiro número: '))
+numero_dois = int(input('Digite o segundo número: '))
+print(f'A soma de {numero_um} e {numero_dois} é: {numero_um + numero_dois}')
